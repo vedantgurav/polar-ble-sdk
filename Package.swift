8f87f0a1187b7f0d35074658600b0796e070fb94
Vedant Gurav <hello@vedantgurav.com>
2024-11-02 20:58:14 +0000

    Fix package dependencies

--- Package.swift
+++ Package.swift
@@ -13,11 +13,14 @@ let package = Package(
     dependencies: [
         .package(name: "RxSwift", url: "https://github.com/ReactiveX/RxSwift.git", .exact("6.5.0")),
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
+        .package(url: "https://github.com/marmelroy/Zip.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1"))
+        
     ],
     targets: [
         .target(
             name: "PolarBleSdk",
-            dependencies: ["SwiftProtobuf", "RxSwift"],
+            dependencies: ["SwiftProtobuf", "RxSwift", "Zip", "Alamofire"],
             path: "sources/iOS/ios-communications/Sources",
             exclude: ["iOSCommunications/Info.plist", "PolarBleSdk/Info.plist"]
         ),
